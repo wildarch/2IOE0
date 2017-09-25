8d6d3188a7dec71760fe3e03eeb3f79fb39d7a65
Geert van Ieperen <ieperen3039@outlook.com>
2017-09-25 10:22:15 +0200

    Added updatable classes

---
+++ src/nl/tue/c2IOE0/group5/engine/UpdatableFloat.java
@@ -0,0 +1,22 @@
+package nl.tue.c2IOE0.group5.engine;
+
+/**
+ * Created by s152717 on 4-5-2017.
+ */
+public class UpdatableFloat extends Updatable<Float> {
+
+    public UpdatableFloat(float initial) {
+        super(initial);
+    }
+
+    public UpdatableFloat(float current, float previous){
+        super(current, previous);
+    }
+
+    /**
+     * @return the increase of the last update, defined as (current - previous)
+     */
+    public float difference() {
+        return current() - previous();
+    }
+}
