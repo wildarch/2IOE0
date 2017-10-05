84ad7141422038e4b0f5b87c00df8ab68dfa2164
Daan Drijver <d.drijver@student.tue.nl>
2017-10-05 16:02:23 +0200

    Added Camera controls, hold down middle mouse button and move to move the camera.
    Signed-off-by: Daan Drijver <d.drijver@student.tue.nl>

--- src/nl/tue/c2IOE0/group5/controllers/TestController.java
+++ src/nl/tue/c2IOE0/group5/controllers/TestController.java
@@ -18,6 +18,18 @@ public class TestController implements Controller,Listener {
     // define required resources here, e.g.
     private TestProvider testProvider;
     private Camera camera;
+    private float oldx = 0;
+    private float oldy = 0;
+    private float sensitivity = 5;//Camera Sensitivity on a scale from 1 to 10
+
+    private boolean MiddleMouseButton = false;
+    private boolean W = false;
+    private boolean A = false;
+    private boolean S = false;
+    private boolean D = false;
+    private boolean R = false;
+    private boolean F = false;
+
 
     @Override
     public void init(Engine engine) {
@@ -29,7 +41,24 @@ public class TestController implements Controller,Listener {
     @Override
     public void update() {
         // you can use resources here, e.g.
-
+        if (A){
+            camera.moveRelative(-0.1f, 0f, 0f);
+        }
+        if (D){
+            camera.moveRelative(0.1f, 0f, 0f);
+        }
+        if (W){
+            camera.moveRelative(0f, 0f, -0.1f);
+        }
+        if (S){
+            camera.moveRelative(0f, 0f, 0.1f);
+        }
+        if (R){
+            camera.moveRelative(0f, 0.1f, 0f);
+        }
+        if (F){
+            camera.moveRelative(0f, -0.1f, 0f);
+        }
     }
 
     @Override
@@ -37,27 +66,64 @@ public class TestController implements Controller,Listener {
 
         switch (event.getSubject()) {
             case GLFW_KEY_A:
-                //camera.setRotation(0, 90, 0);
-                camera.moveRelative(-1f, 0f, 0f);
+                A = true;
                 break;
             case GLFW_KEY_D:
-                //camera.setRotation(0, -90, 0);
-                camera.moveRelative(1f, 0f, 0f);
+                D = true;
                 break;
             case GLFW_KEY_S:
-                //camera.setRotation(0, 180, 0);
-                camera.moveRelative(0f, 0f, 1f);
+                S = true;
                 break;
             case GLFW_KEY_W:
-                //camera.setRotation(0, 0, 0);
-                camera.moveRelative(0f, 0f, -1f);
+                W = true;
+                break;
+            case GLFW_KEY_F:
+                F = true;
+                break;
+            case GLFW_KEY_R:
+                R = true;
+                break;
+            case GLFW_KEY_L:
+                camera.setRotation(0,0,0);
+                break;
+            case GLFW_KEY_T:
+                float xRotation = (camera.getRotation().x());
+                float yRotation = (camera.getRotation().y())%360;
+
+                float unitVX = (float)Math.sin(yRotation*Math.PI/180);
+                float unitVY = (float)Math.sin(xRotation*Math.PI/180);
+                float unitVZ = (float)Math.cos(xRotation*Math.PI/180);
+
+                System.out.println("X: " + xRotation);
+                System.out.println("Y: " + yRotation);
+
+                camera.moveRelative(unitVX/2,-unitVY/2,-unitVZ/2);
                 break;
         }
     }
 
     @Override
     public void onKeyReleased(Event event) {
-
+        switch (event.getSubject()) {
+            case GLFW_KEY_A:
+                A = false;
+                break;
+            case GLFW_KEY_D:
+                D = false;
+                break;
+            case GLFW_KEY_S:
+                S = false;
+                break;
+            case GLFW_KEY_W:
+                W = false;
+                break;
+            case GLFW_KEY_R:
+                R = false;
+                break;
+            case GLFW_KEY_F:
+                F = false;
+                break;
+        }
     }
 
     @Override
@@ -66,16 +132,46 @@ public class TestController implements Controller,Listener {
             System.out.println("Click at (" + event.getX() + ", " + event.getY() + ")");
             this.testProvider.ud();
         }
+
+        if (event.getSubject() == GLFW_MOUSE_BUTTON_2) {
+            //System.out.println("M at (" + event.getX() + ", " + event.getY() + ")");
+            MiddleMouseButton = true;
+
+            /******************************************************************************************\
+            Set current location as the start for the delta X and Y. Otherwise it will use outdated data
+            and think you'll have moveD in-between pressing releasing middle mouse and pressing it again.
+            \******************************************************************************************/
+            oldx = event.getX();
+            oldy = event.getY();
+        }
     }
 
     @Override
     public void onMouseButtonReleased(MouseEvent event) {
-
+        if (event.getSubject() == GLFW_MOUSE_BUTTON_2) {
+            //System.out.println("M at (" + event.getX() + ", " + event.getY() + ")");www
+            MiddleMouseButton = false;
+        }
     }
 
     @Override
     public void onMouseMove(MouseEvent event) {
+        //Get current values
+        if (MiddleMouseButton) {
+            float x = event.getSource().getMousePosition().getX();
+            float y = event.getSource().getMousePosition().getY();
 
+            float deltaxMouse = (x - oldx) * (sensitivity / 10);
+            float deltayMouse = (y - oldy) * (sensitivity / 10);
+
+            if ((camera.getRotation().x() + deltayMouse <= 90) && (camera.getRotation().x() + deltayMouse > -35)){
+                camera.rotate(deltayMouse, deltaxMouse, 0);
+            }
+
+            //Set new old values
+            oldx = x;
+            oldy = y;
+        }
     }
 
 }
