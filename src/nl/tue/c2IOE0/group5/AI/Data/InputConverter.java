f3cccbabd1159c915d66a0f52d2fe92f01a2ca96
Tim Beurskens <timbeurskens.97@gmail.com>
2017-10-19 15:43:05 +0200

    create two way input converter

--- src/nl/tue/c2IOE0/group5/AI/Data/InputConverter.java
+++ src/nl/tue/c2IOE0/group5/AI/Data/InputConverter.java
@@ -7,26 +7,72 @@ import nl.tue.c2IOE0.group5.towers.TowerType;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * TowerDefence
  * Created by s154796 on 5-10-2017.
  */
 public class InputConverter {
-    private final EnemyType[] buffer;
-    private final GridProvider grid;
-    private final double qTrust;
+    enum ConverterDirection {
+        GameToAI,
+        AIToGame
+    }
 
-    public InputConverter(GridProvider grid, double qTrust, EnemyType... buffer){
-        this.buffer = buffer;
-        this.grid = grid;
-        this.qTrust = qTrust;
+    private int gridSize, nrTowers, nrDeployTypes, bufferSize;
+
+    private EnemyType[] buffer;
+    private GridProvider grid;
+    private TowerType[][] tGrid;
+    private double qTrust;
+    private INDArray input;
+
+    private final ConverterDirection direction;
+
+    public static InputConverter fromNNInput(INDArray row, int gridSize, int nrTowers, int nrDeployTypes, int bufferSize){
+        InputConverter converter = new InputConverter(ConverterDirection.AIToGame);
+        converter.gridSize = gridSize;
+        converter.nrTowers = nrTowers;
+        converter.nrDeployTypes = nrDeployTypes;
+        converter.bufferSize = bufferSize;
+        converter.input = row;
+        converter.convert();
+        return converter;
+    }
+
+    public static InputConverter fromGameState(GridProvider grid, double qTrust, EnemyType... buffer){
+        InputConverter converter = new InputConverter(ConverterDirection.GameToAI);
+        converter.buffer = buffer;
+        converter.grid = grid;
+        converter.qTrust = qTrust;
+        converter.gridSize = grid.SIZE;
+        converter.nrTowers = TowerType.getSize();
+        converter.nrDeployTypes = EnemyType.getSize();
+        converter.bufferSize = buffer.length;
+        converter.convert();
+        return converter;
+    }
+
+    private void convert(){
+        switch (direction){
+            case GameToAI:
+                convertGameToAI();
+                break;
+            case AIToGame:
+                convertAIToGame();
+                break;
+        }
+    }
+
+    private InputConverter(ConverterDirection direction){
+        this.direction = direction;
     }
 
     /**
      * Convert game objects to NN input neurons
-     * @return INDArray input row vector
      */
-    public INDArray convert() {
+    private void convertGameToAI() {
         double[] result = new double[getGridInputSize() + getBufferInputSize() + getQTrustInputSize()];
 
         double[] gridResult = convertGrid();
@@ -37,15 +83,66 @@ public class InputConverter {
         System.arraycopy(bufferResult, 0, result, getGridInputSize(), getBufferInputSize());
         System.arraycopy(qResult, 0, result, getGridInputSize() + getBufferInputSize(), getQTrustInputSize());
 
-        return Nd4j.create(result);
+        input = Nd4j.create(result);
+    }
+
+    private void convertAIToGame(){
+        tGrid = new TowerType[gridSize][gridSize];
+        generateGrid();
+        buffer = new EnemyType[0];
+        generateBuffer();
+        qTrust = input.getDouble(0, getGridInputSize() + getBufferInputSize());
+    }
+
+    private void generateGrid(){
+        for (int x = 0; x < gridSize; x++){
+            for (int y = 0; y < gridSize; y++){
+                for (int i = 0; i < nrTowers; i++){
+                    if (input.getDouble(0, x * gridSize * nrTowers + y * nrTowers + i) > 0)
+                        tGrid[x][y] = TowerType.values()[i];
+                }
+            }
+        }
+    }
+
+    private void generateBuffer() {
+        List<EnemyType> bufferList = new ArrayList<>(bufferSize);
+        for (int i = 0; i < nrDeployTypes; i++){
+            int pos = getGridInputSize() + i;
+            int count = (int) Math.floor(input.getDouble(0, pos) * bufferSize);
+            for (int j = 0; j < count; j++){
+                bufferList.add(EnemyType.values()[i]);
+            }
+        }
+        buffer = bufferList.toArray(buffer);
+    }
+
+    public EnemyType[] getBuffer(){
+        if (direction != ConverterDirection.AIToGame) throw new RuntimeException("illegal request");
+        return buffer;
+    }
+
+    public TowerType[][] getGrid(){
+        if (direction != ConverterDirection.AIToGame) throw new RuntimeException("illegal request");
+        return tGrid;
+    }
+
+    public double getqTrust(){
+        if (direction != ConverterDirection.AIToGame) throw new RuntimeException("illegal request");
+        return qTrust;
+    }
+
+    public INDArray getInput(){
+        if (direction != ConverterDirection.GameToAI) throw new RuntimeException("illegal request");
+        return input;
     }
 
     /**
      * get the number of neurons needed for the buffer input
      * @return int nr. of neurons
      */
-    public static int getBufferInputSize(){
-        return TowerType.getSize();
+    public int getBufferInputSize(){
+        return nrDeployTypes;
     }
 
     /**
@@ -53,7 +150,7 @@ public class InputConverter {
      * @return int nr. of neurons
      */
     public int getGridInputSize(){
-        return grid.SIZE * grid.SIZE * TowerType.getSize();
+        return gridSize * gridSize * nrTowers;
     }
 
     /**
@@ -69,13 +166,12 @@ public class InputConverter {
      * @return array input neurons
      */
     private double[] convertGrid(){
-        final int numTowers = TowerType.getSize();
-        double[] result = new double[grid.SIZE * grid.SIZE * numTowers];
+        double[] result = new double[gridSize * gridSize * nrTowers];
         int positionId, towerId;
         AbstractTower tower;
-        for (int x = 0; x < grid.SIZE; x++){
-            for (int y = 0; y < grid.SIZE; y++){
-                positionId = x * grid.SIZE * numTowers + y * numTowers;
+        for (int x = 0; x < gridSize; x++){
+            for (int y = 0; y < gridSize; y++){
+                positionId = x * gridSize * nrTowers + y * nrTowers;
                 tower = grid.getCell(x, y).getTower();
                 towerId = tower.getType().getValue();
                 result[positionId + towerId] = tower.getLevel();
@@ -89,8 +185,7 @@ public class InputConverter {
      * @return array input neurons
      */
     private double[] convertBuffer(){
-        double totalUnits = buffer.length;
-        double[] result = new double[EnemyType.getSize()];
+        double[] result = new double[nrDeployTypes];
 
         //count the number of units
         for (EnemyType t : buffer){
@@ -99,9 +194,11 @@ public class InputConverter {
 
         //only divide at the end to preserve decimal precision
         for (int i = 0; i < result.length; i++){
-            result[i] /= totalUnits;
+            result[i] /= bufferSize;
         }
 
         return result;
     }
+
+
 }
